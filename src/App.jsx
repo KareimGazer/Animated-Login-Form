27976a31ce6e1ef6d0cb706f6fbb187aa582288f
KareimGazer <kareimgazar1990@gmail.com>
2024-12-02 23:47:51 +0200

    batman

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import LoginForm from './LoginForm'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Login Page</h1>
+      <main>
+        <LoginForm/>
+      </main>
+    </>
+  )
+}
+
+export default App
